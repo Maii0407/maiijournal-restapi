b5b0d8e9a8199e9921b990fb39160c9a4648f872
Akmal Izuddin <izuddin0407@gmail.com>
2022-07-25 10:40:36 +0800

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'myJournal-restAPI' });
+  res.render('index', { title: 'maiiJournal-restAPI' });
 });
 
 module.exports = router;
